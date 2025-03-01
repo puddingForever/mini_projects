663efc47f6fbaa98ed85a45edca775cfd2f935dc
BytePudding <developerjieun94@gmail.com>
2025-03-01 16:53:53 +0900

    movie booking

---
+++ movie_seat_booking/script.js
@@ -0,0 +1,74 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+const img = document.querySelector("img");
+const imageLists = [
+  "./assets/aladdin.gif",
+  "./assets/shrek.gif",
+  "./assets/pocahontas.gif",
+];
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+// localStorage 저장
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+// update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeaetsCount = selectedSeats.length;
+  count.innerText = selectedSeaetsCount;
+  total.innerText = selectedSeaetsCount * ticketPrice;
+}
+
+// get data from localStorage
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+  img.src = imageLists[e.target.selectedIndex];
+});
+
+// 좌석 예약 이벤트
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
+
+// count ,total 초기화
+updateSelectedCount();
